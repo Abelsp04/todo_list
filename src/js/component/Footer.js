0670ccd516837110082f7859dd8039c268d525fc
Abel Segui <Abelsegui@hotmail.com>
2020-05-02 03:52:16 +0000

    footer

---
+++ src/js/component/Footer.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+function Footer() {
+	return (
+		<footer class="page-footer font-small pt-4">
+			<h5>TodoList Copyright</h5>
+		</footer>
+	);
+}
+
+export default Footer;
--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,10 +1,9 @@
 import React from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 import TodoList from "./TodoList";
 import Navbar from "./Navbar.js";
-
+import Footer from "./Footer.js";
 //create your first component
 export function Home() {
 	return (
@@ -14,6 +13,10 @@ export function Home() {
 			</div>
 			<div className="body">
 				<TodoList />
+			</div>
+
+			<div className="footer">
+				<Footer />
 			</div>
 		</div>
 	);
